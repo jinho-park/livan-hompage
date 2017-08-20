74aa1f6147352c1817bd29ff62777689c7ddfa30
unknown <lucky112233335@gmail.com>
2017-08-20 11:23:56 +0900

    scss module apply

---
+++ livan-client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Livan's Homepage
+            </div>
+        )
+    }
+}
+
+export default App;
